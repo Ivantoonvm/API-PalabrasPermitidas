394db8d599970b8c49e7b6f5086252479bb08f56
Ivan Velazquez <98299885+Ivantoonvm@users.noreply.github.com>
2022-03-12 14:04:00 -0600

    Script para base de datos de MongoDB

---
+++ ScriptMessage.js
@@ -0,0 +1,16 @@
+use message;
+db.messages.insert({
+    "word": "Hola mama",
+    "allowed": false,
+    "status": 'active',
+    "dateCreated": new Date(),
+    "lastDateUpdate": new Date(),
+    "dateDeleted": new Date(),
+});
+db.messages.insert({
+    "word": "Buenos dias",
+    "allowed": true,
+    "status": 'delated',
+    "dateCreated": new Date(),
+    "lastDateUpdate": new Date(),
+});
